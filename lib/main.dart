dbd4553b6f9abfa3451e3fabfda9a773dfc19718
JovanNathanielJie <jovannathanieljie_2327240013@mhs.mdp.ac.id>
2026-04-22 15:03:44 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fasum_jovannathanieljie/screens/sign_up_screen.dart';
+import 'package:fasum_jovannathanieljie/screens/sign_in_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
