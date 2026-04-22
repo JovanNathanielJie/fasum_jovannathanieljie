8b2a15dfb1a8c72ab8f19e769afb4f7723f1d933
JovanNathanielJie <jovannathanieljie_2327240013@mhs.mdp.ac.id>
2026-04-22 14:30:16 +0700

    Update sign_up_screen.dart

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fasum_jovannathanieljie/screens/home_screen.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -18,6 +19,72 @@ class _SignUpScreenState extends State<SignUpScreen> {
   bool _isLoading = false;
   bool _isPasswordVisible = false;
   bool _isConfirmPasswordVisible = false;
+
+  void _signUp() async{
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+    setState(() 
+      => _isLoading = true
+    );
+    try {
+      final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      await FirebaseFirestore.instance.collection('users').doc(userCredential.user!.uid).set({
+        'fullName': _fullNameController.text.trim(),
+        'email': email,
+        'createdAt': Timestamp.now(),
+      });
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+        (route) => false, //hapus semua route sebelumnya
+      );
+    } on FirebaseAuthException catch (error) {
+      _showErrorMessage(_getAuthErrorMessage(error.code));
+    } catch (error) {
+      _showErrorMessage('An unexpected error occurred: {$error}. Please try again later.');
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
+
+  void _showErrorMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  bool _isValidEmail (String email) {
+    String emailRegex = r'^[a-zA-Z0-9.!#$%&*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$';
+    return RegExp(emailRegex).hasMatch(email);
+  }
+
+  String _getAuthErrorMessage(String errorCode) {
+    switch (errorCode) {
+      case 'email-already-in-use':
+        return 'The email is already in use. Please try another one.';
+      case 'invalid-email':
+        return 'The email address is not valid. Please enter a valid email.';
+      case 'weak-password':
+        return 'The password is too weak. Please choose a stronger password.';
+      default:
+        return 'An unexpected error occurred. Please try again later.';
+    }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _fullNameController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +123,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       prefixIcon: Icon(Icons.email),
                     ),
                     validator: (value) {
-                      if (value == null || value.isEmpty) || !_isValidEmail(value)) {
+                      if (value == null || value.isEmpty || !_isValidEmail(value)) {
                         return 'Please enter a valid email';
                       }
                       return null;
@@ -65,30 +132,53 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   const SizedBox(height: 16.0),
                   TextFormField(
                     controller: _passwordController,
-                    decoration: const InputDecoration(
+                    decoration: InputDecoration(
                       labelText: 'Password',
-                      border: OutlineInputBorder(),
+                      border: const OutlineInputBorder(),
                       prefixIcon: Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isPasswordVisible = !_isPasswordVisible;
+                          });
+                        },
                     ),
-                    obscureText: !_isPasswordVisible,
-                    validator: (value) {
+                    ),
+                   obscureText: !_isPasswordVisible,
+                   validator: (value) {
                       if (value == null || value.trim().isEmpty) {
-                        return 'Please enter your password';
+                        return 'Please enter a password';
                       }
-                      if (value.length < 6) {
-                        return 'Password must be at least 6 characters long';
+                      if (value.trim().length < 6) {
+                        return 'Password must be at least 6 characters';
                       }
                       return null;
                     },
-
                   ),
                   const SizedBox(height: 16.0),
                   TextFormField(
                     controller: _confirmPasswordController,
-                    decoration: const InputDecoration(
+                    decoration: InputDecoration(
                       labelText: 'Confirm Password',
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.lock),
+                      border: const OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock_outline),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isConfirmPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+                          });
+                        },
+                    ),
                     ),
                     obscureText: !_isConfirmPasswordVisible,
                     validator: (value) {
@@ -102,6 +192,12 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     },
                   ),
                   const SizedBox(height: 16.0),
+                  _isLoading
+                    ? const CircularProgressIndicator()
+                    : ElevatedButton(
+                      onPressed: _signUp,
+                      child: const Text('Sign Up'),
+                    ),
                 ],
               ),
             ),
