1e2c5c0aa0395004016679d91f4e145e7cdc9895
JovanNathanielJie <jovannathanieljie_2327240013@mhs.mdp.ac.id>
2026-04-29 14:16:32 +0700

    add post screen + imagepicker

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -8,7 +10,80 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
-  
+  File? _image;
+  String? _based64Image;
+  final TextEditingController _descriptionController = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
+  bool _isUploading = false;
+  double? _latitude;
+  double? _longitude;
+  String? _aiCategory;
+  String? _aiDescription;
+  bool _isGenerating = false;
+  List<String> categories = [
+    'Jalan Rusak',
+    'Marka Pudar',
+    'Lampu Mati',
+    'Trotoar Rusak',
+    'Rambu Rusak',
+    'Jembatan Rusak',
+    'Sampah Menumpuk',
+    'Saluran Tersumbat',
+    'Sungai Tercemar',
+    'Sampah Sungai',
+    'Pohon Tumbang',
+    'Taman Rusak',
+    'Fasilitas Rusak',
+    'Pipa Bocor',
+    'Vandalisme',
+    'Banjir',
+    'Lainnya',    
+  ];
+
+  void _showCategorySelection() {
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+      ),
+      builder: (BuildContext context) {
+        return ListView(
+          shrinkWrap: true,
+          children:
+          categories.map((category) {
+            return ListTile(
+              title: Text(category),
+              onTap: () {
+                setState(() {
+                  _aiCategory = category; // Ganti AI category dengan pilihan user
+                });
+                Navigator.pop(context);
+              },
+            );
+          }).toList()
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _pickImage(ImageSource source) async {
+
+  }
+
+  Future<void> _compressAndEncodeImage() async {
+
+  }
+
+  Future<void> _generateDescriptionWithAI() async {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
