76ff2f140e18972812a3ee266d4dec95cbc71ea6
JovanNathanielJie <jovannathanieljie_2327240013@mhs.mdp.ac.id>
2026-05-13 15:05:34 +0700

    Update detail_screen.dart

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatefulWidget {
-  const DetailScreen({super.key});
+  final String imageBase64;
+  final String description;
+  final DateTime createdAt;
+  final String fullName;
+  final double latitude;
+  final double longitude;
+  final String category;
+  final String heroTag;
+
+  const DetailScreen({
+    super.key,
+    required this.imageBase64,
+    required this.description,
+    required this.createdAt,
+    required this.fullName,
+    required this.latitude,
+    required this.longitude,
+    required this.category,
+    required this.heroTag,
+  });
 
   @override
   State<DetailScreen> createState() => _DetailScreenState();
